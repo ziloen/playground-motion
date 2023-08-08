5bc4aa5d18ea241219d87d25440af8c76e46df25
N <34268416+ziloen@users.noreply.github.com>
2023-08-08 14:20:29 +0800

    feat: add stagger animate on index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
+import { stagger } from 'framer-motion'
 import { NavLink, RouteObject } from 'react-router-dom'
 import routes from '~react-pages'
 
 export default function Index() {
-  // const routes = useRoutes()
-
   const flattenedRoutes = useMemo(() => {
     return routes
       .reduce<RouteObject[]>((acc, route) => {
@@ -18,8 +17,15 @@ export default function Index() {
     <div className='px-20px pt-40px'>
       <h1 className="text-2xl font-bold">Routes</h1>
       <div className="flex flex-col items-start gap-4px mt-12px">
-        {flattenedRoutes.map(route =>
-          <NavLink key={route} to={route}>{route}</NavLink>
+        {flattenedRoutes.map((route, i) =>
+          <motion.div
+            key={route}
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: i * .08 }}
+          >
+            <NavLink key={route} to={route}>{route}</NavLink>
+          </motion.div>
         )}
       </div>
     </div>
