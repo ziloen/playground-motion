4e58ffdd9c0a684c34556857c0de793b8fd218d4
N <34268416+ziloen@users.noreply.github.com>
2023-08-24 17:36:39 +0800

    chore: revert LayoutNoScale

--- src/components/LayoutNoScale.tsx
+++ src/components/LayoutNoScale.tsx
@@ -1,90 +1,46 @@
-import { useLatest } from 'ahooks'
-import { ValueAnimationTransition, animate, useMotionValue } from 'framer-motion'
+import { useAnimation } from 'framer-motion'
 import { type PropsWithChildren } from 'react'
 import { useResizeObserver } from '~/hooks'
 
 type Props = PropsWithChildren<{
   layoutDependency?: unknown
-  transition?: ValueAnimationTransition | undefined
 }>
 
-
-/** 
- * Prevent layout scale during layout animation.
+/**
+ * Prevents scale of children during layout animation
+ * 
+ * TODO: Only animate width and height when laout animation is triggered
  */
 export function LayoutNoScale({
   children,
-  layoutDependency,
-  transition
+  layoutDependency
 }: Props) {
-  /** container element */
   const ref = useRef<HTMLDivElement>(null)
-  /** is container layout animating */
-  const layoutAnimating = useRef(false)
-  type Size = '100%' | number
-  /** child width */
-  const width = useMotionValue<Size>('100%')
-  /** child height */
-  const height = useMotionValue<Size>('100%')
-  /** container size */
-  const containerSize = useRef({ w: '100%' as Size, h: '100%' as Size })
-
-  const transitionRef = useLatest(transition)
+  const controls = useAnimation()
 
   useResizeObserver(ref, ([entry]) => {
     if (!entry) return
     const size = entry.contentBoxSize[0]
     if (!size) return
 
-    const { inlineSize: w, blockSize: h } = size
+    const { inlineSize, blockSize } = size
 
-    setSize: {
-      // If layout is not animating, skip animation
-      if (!layoutAnimating.current) break setSize
-
-      width.stop()
-      height.stop()
-      const options: ValueAnimationTransition = transitionRef.current || { type: 'tween', duration: .3 }
-      animate(width, w, options)
-        .then(() => {
-          if (layoutAnimating.current) return
-          width.set('100%')
-        })
-      animate(height, h, options)
-        .then(() => {
-          if (layoutAnimating.current) return
-          height.set('100%')
-        })
-    }
-
-    // save container size
-    containerSize.current = { w, h }
+    controls.start({
+      width: inlineSize,
+      height: blockSize,
+    })
   })
-
-  function onLayoutAnimationStart() {
-    layoutAnimating.current = true
-    const { w: lw, h: lh } = containerSize.current
-    // set last size before container size change
-    width.set(lw)
-    height.set(lh)
-  }
-
-  function onLayoutAnimationComplete() {
-    layoutAnimating.current = false
-  }
 
   return (
     <motion.div
       layout="position"
       layoutDependency={layoutDependency}
-      onLayoutAnimationStart={onLayoutAnimationStart}
-      onLayoutAnimationComplete={onLayoutAnimationComplete}
       ref={ref}
       className="h-full h-full relative"
     >
       <motion.div
-        className='absolute'
-        style={{ width, height }}
+        className='absolute h-full w-full'
+        animate={controls}
       >
         {children}
       </motion.div>
