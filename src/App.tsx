211c1bbe22f3ac2d8d0ffe17b52e0b64001c656c
N <34268416+ziloen@users.noreply.github.com>
2023-10-20 11:50:51 +0800

    fix: nest route error & update deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,34 @@
 import { MotionConfig } from 'framer-motion'
 import { Suspense } from 'react'
 import {
-  HashRouter,
-  Route,
-  Routes,
-  useLocation
+  HashRouter
 } from 'react-router-dom'
 import routes from '~react-pages'
 
-const routeElements = routes.map(route => (
-  <Route
-    key={route.path}
-    path={route.path}
-    element={route.element}
-  />
-))
 
-function App() {
-  const location = useLocation()
-
-  return (
-    <MotionConfig transition={{ type: 'tween' }}>
-      <Suspense>
-        <AnimatePresence mode="wait" initial={false}>
-          <Routes location={location} key={location.pathname}>
-            {routeElements}
-          </Routes>
-        </AnimatePresence>
-      </Suspense>
-    </MotionConfig>
-  )
+function Routes() {
+  return useRoutes(routes)
 }
 
-
-export default function AppWithRouter() {
+export default function App() {
   return (
     <HashRouter>
-      <App />
+      <MotionConfig transition={{ type: 'tween' }}>
+        <Suspense>
+          <AnimatePresence mode="wait" initial={false}>
+            <Routes />
+          </AnimatePresence>
+        </Suspense>
+      </MotionConfig>
     </HashRouter>
   )
 }
+
+
+// export default function AppWithRouter() {
+//   return (
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   )
+// }
