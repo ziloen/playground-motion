5f0ec315547ba3963f04345245202eb37974b121
N <34268416+ziloen@users.noreply.github.com>
2023-11-15 06:14:23 +0800

    chore: update to flat eslint config

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+import { format, react } from '@ziloen/eslint-config'
+
+/** @type { import("@ziloen/eslint-config").FlatESLintConfigItem[] } */
+export default [
+  ...react({ tsconfigPath: './tsconfig.json' }),
+  ...format
+]
