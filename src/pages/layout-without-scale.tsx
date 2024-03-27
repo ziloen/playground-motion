8dca05065fe1a86011b7230c35e68ea8e5b35845
N <34268416+ziloen@users.noreply.github.com>
2024-03-27 16:14:14 +0800

    Pin framer-motion at 11.0.8

--- src/pages/layout-without-scale.tsx
+++ src/pages/layout-without-scale.tsx
@@ -1,19 +1,7 @@
-import { useAnimation } from 'framer-motion'
 import { LayoutNoScale } from '~/components'
-import { useResizeObserver } from '~/hooks'
 
 export default function LayoutWithoutScale() {
   const [show, setShow] = useState(false)
-  const ref = useRef<HTMLDivElement>(null)
-  const controls = useAnimation()
-
-  useResizeObserver(ref, ([entry]) => {
-    if (!entry) return
-    const { inlineSize, blockSize } = entry.contentBoxSize[0]!
-    controls.start({ width: inlineSize, height: blockSize })
-  })
-
-
 
   return (
     <motion.div
@@ -27,25 +15,26 @@ export default function LayoutWithoutScale() {
         className='resizable w-[700px] h-[400px] bg-neutral grid gap-[12px]'
         style={{ gridTemplateColumns: 'minmax(0, 1fr) minmax(0, 1fr)', gridTemplateRows: 'minmax(0, 1fr)' }}
       >
-        <AnimatePresence>
+        <AnimatePresence
+          initial={false}
+        >
           {show && (
-            <motion.div
+            <motion.img
               layout
               key="1"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              className='bg-emerald z-1'
+              className='bg-emerald z-1 object-contain object-center w-full h-full'
+              src='https://images.unsplash.com/photo-1691250993170-4c9919194aa4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=794&q=80'
               style={{ gridColumn: '1 / 2', gridRow: '1/2' }}
-            >
-              <img src='https://images.unsplash.com/photo-1691250993170-4c9919194aa4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=794&q=80' className='w-full h-full object-contain object-center' />
-            </motion.div>
+            />
           )}
 
           {/* Out container will be scaled cause layout change */}
           <motion.div
-            layout
             key="2"
+            layout
             layoutDependency={show}
             className='bg-blue z-2 h-full w-full'
             style={{ gridColumn: show ? '2 / 3' : '1 / 3', gridRow: '1 / 2' }}
@@ -60,6 +49,7 @@ export default function LayoutWithoutScale() {
           </motion.div>
         </AnimatePresence>
       </div>
+
       <button className='btn' onClick={() => setShow(s => !s)}>Toggle</button>
 
       <div
