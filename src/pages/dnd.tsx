1f5529e264945c5165df4cc51628ad2bea15080b
N <34268416+ziloen@users.noreply.github.com>
2024-09-18 14:58:24 +0800

    update dnd page

--- src/pages/dnd.tsx
+++ src/pages/dnd.tsx
@@ -1,11 +1,27 @@
+const MAX_SCALE = 10
+const MIN_SCALE = 0.3
+
 export default function DND() {
+  const [scale, setScale] = useState(1)
+
+  // scale from mouse position
+  // 
+
   return (
     <motion.div
       drag
       dragMomentum={false}
-      // dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+      style={{ scale }}
+      dragConstraints={useRef(document.body)}
       dragElastic={1}
+      onWheel={e => {
+        if (e.deltaY > 0) {
+          setScale(scale => Math.min(Math.max(scale - 0.3, MIN_SCALE), MAX_SCALE))
+        } else {
+          setScale(scale => Math.min(Math.max(scale + 0.3, MIN_SCALE), MAX_SCALE))
+        }
+      }}
       className="size-[100px] rounded-full bg-green-700"
-    ></motion.div>
+    />
   )
 }
