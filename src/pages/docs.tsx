8789ebb12ce847110fd34f84b980f16dc7ff28ce
N <34268416+ziloen@users.noreply.github.com>
2024-12-02 19:02:01 +0800

    remove antd

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,7 +1,5 @@
 // https://github.com/remorses/framer-motion-visualizer/blob/main/website/src/pages/home.tsx
 
-import { ConfigProvider, Segmented, Select, theme } from 'antd'
-
 export default function Docs() {
   return (
     <motion.div>
@@ -11,55 +9,47 @@ export default function Docs() {
           type: 'tween',
           ease: '',
         }}
-      ></motion.div>
+      />
 
-      <ConfigProvider
-        theme={{
-          algorithm: theme.darkAlgorithm,
-          token: {
-            motion: false,
-          },
+      <div
+        className="grid gap-y-2"
+        style={{
+          gridTemplateColumns:
+            '[title-start main-start] minmax(60px,max-content) [title-end input-start] 1fr [input-end main-end]',
         }}
       >
-        <div
-          className="grid gap-y-2"
-          style={{
-            gridTemplateColumns:
-              '[title-start main-start] minmax(60px,max-content) [title-end input-start] 1fr [input-end main-end]',
-          }}
-        >
-          <div className="col-[title]">type:</div>
-          <Segmented
-            className="col-[input]"
-            options={['inertia', 'just', 'keyframes', 'spring', 'tween']}
-          />
+        <div className="col-[title]">type:</div>
+        {['inertia', 'just', 'keyframes', 'spring', 'tween'].map((v) => (
+          <span className="col-[input]">{v}</span>
+        ))}
 
-          <div className="col-[title]">mode:</div>
-          <Segmented className="col-[input]" options={['mass', 'duration']} />
+        <div className="col-[title]">mode:</div>
+        {['mass', 'duration'].map((v) => (
+          <span className="col-[input]">{v}</span>
+        ))}
 
-          <div className="col-[title]">ease:</div>
-          <Select
-            options={[
-              'linear',
-              'easeIn',
-              'easeOut',
-              'easeInOut',
-              'circIn',
-              'circOut',
-              'circInOut',
-              'backIn',
-              'backOut',
-              'backInOut',
-              'anticipate',
-            ].map(v => ({
-              value: v,
-              label: v,
-            }))}
-            popupMatchSelectWidth={100}
-            className="col-[input] w-fit"
-          />
-        </div>
-      </ConfigProvider>
+        <div className="col-[title]">ease:</div>
+
+        <select className="col-[input] w-fit">
+          {[
+            'linear',
+            'easeIn',
+            'easeOut',
+            'easeInOut',
+            'circIn',
+            'circOut',
+            'circInOut',
+            'backIn',
+            'backOut',
+            'backInOut',
+            'anticipate',
+          ].map((v) => (
+            <option key={v} value={v}>
+              {v}
+            </option>
+          ))}
+        </select>
+      </div>
     </motion.div>
   )
 }
