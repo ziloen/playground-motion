700a4ffcfe341faea19d4498cf07c0c19fe3413e
N <34268416+ziloen@users.noreply.github.com>
2025-04-10 20:39:23 +0800

    update deps

--- src/components/AutoHeightPanel.tsx
+++ src/components/AutoHeightPanel.tsx
@@ -1,15 +1,18 @@
+import type { Transition } from 'motion/react'
 import { useAnimation } from 'motion/react'
 import type { PropsWithChildren } from 'react'
 
 type Props = PropsWithChildren<{
   className?: string
   innerClassName?: string
+  transition?: Transition
 }>
 
 export function AutoHeightPanel({
   children,
   className,
   innerClassName,
+  transition,
 }: Props) {
   const ref = useRef<HTMLDivElement>(null!)
   const controls = useAnimation()
@@ -32,7 +35,7 @@ export function AutoHeightPanel({
   return (
     <motion.div
       animate={controls}
-      transition={{ type: 'tween', duration: 0.2 }}
+      transition={transition ?? { type: 'tween', duration: 0.2 }}
       className={className}
     >
       <div ref={ref} className={innerClassName}>
