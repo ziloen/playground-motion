affd3c6d9c7bfc2bf1ebc79c565e682d655cc1c4
N <34268416+ziloen@users.noreply.github.com>
2025-07-11 16:47:06 +0800

    remove redunt code

--- react-router.config.js
+++ react-router.config.js
@@ -11,7 +11,7 @@ let basename = env.VITE_BASE_URL
 // https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
 if (process.env.GITHUB_REPOSITORY) {
   const slashIndex = process.env.GITHUB_REPOSITORY.indexOf('/')
-  basename = process.env.GITHUB_REPOSITORY.slice(slashIndex) + "/"
+  basename = process.env.GITHUB_REPOSITORY.slice(slashIndex)
   basename.endsWith('/') || (basename += '/')
 }
 
