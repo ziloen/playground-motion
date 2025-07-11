7d429ad8030e40f4771030db25476645a036fab1
N <34268416+ziloen@users.noreply.github.com>
2025-07-11 16:31:17 +0800

    ignore react router generated files

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,5 @@ import { react } from '@ziloen/eslint-config'
 /** @type { import("@ziloen/eslint-config").FlatESLintConfig[] } */
 export default [
   ...react({ project: ['./tsconfig.json', './tsconfig.node.json'] }),
-  { ignores: ['public'] },
+  { ignores: ['public', ".react-router"] },
 ]
