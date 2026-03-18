0d00550155b47f4e2e4e12f9471058b4e2698862
N <34268416+ziloen@users.noreply.github.com>
2026-03-18 17:57:57 +0800

    chore: add jsdoc

--- src/hooks/useTransformState.ts
+++ src/hooks/useTransformState.ts
@@ -1,6 +1,16 @@
 import { useTransform } from 'motion/react'
 import { useMotionValueState } from './useMotionValueState'
 
+/**
+ * Same as `useTransform`, but returns the state of the transformed value instead of a MotionValue.
+ *
+ * @example
+ * ```tsx
+ * const x = useMotionValue(0) // MotionValue<number>
+ * const doubled = useTransform(() => x * 2) // MotionValue<number>
+ * const doubledState = useTransformState(() => x * 2) // number
+ * ```
+ */
 export function useTransformState<T>(transformer: () => T): T {
   return useMotionValueState(useTransform(transformer))
 }
